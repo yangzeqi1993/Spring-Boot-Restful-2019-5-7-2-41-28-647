c63742ca48afb17ffab4f61c64c0478224153f94
yang <yangzq19931003@outlook.com>
2019-07-26 11:18:39 +0800

    提交作业

--- src/main/java/com/example/SpringBootRestful/controller/UserController.java
+++ src/main/java/com/example/SpringBootRestful/controller/UserController.java
@@ -9,34 +9,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping(value="")     // 通过这里配置使下面的映射都在/users下
 //ToDo:在上一行代码("")中填写合适的RestfulApi
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("")
+    @GetMapping(value="/")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity getAllUser() {
         return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
     }
 
-    @PostMapping("")
+    @PostMapping(value="/user")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity addUser(@RequestBody User user) {
         userService.addUser(user);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @PutMapping("")
+    @PutMapping(value="/{userId}")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity updateUserById(@PathVariable int userId, @RequestBody User user) throws BusinessException {
         userService.updateUserById(userId, user);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("")
+    @DeleteMapping(value="/{userId}")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity deleteUserById(@PathVariable int userId) throws BusinessException {
         userService.deleteUserById(userId);
